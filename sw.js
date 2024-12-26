ab0426f59ee899635df6b78d29422f7280de63f1
Luca Spoldi <spoldi.luca06@outlook.it>
2024-12-26 11:46:34 +0100

    removd unecessary logs

--- sw.js
+++ sw.js
@@ -32,18 +32,15 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     (async () => {
       if (e.request.url.includes('pwaversion.txt')) {
-        console.log(`[Service Worker] Fetching version info: ${e.request.url}`);
         return fetch(e.request);
       } else {
         const cachedResponse = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (cachedResponse) {
           return cachedResponse;
         }
         try {
           const response = await fetch(e.request);
           const cache = await caches.open(cacheName);
-          console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
           cache.put(e.request, response.clone());
           return response;
         } catch (error) {
@@ -57,14 +54,12 @@ self.addEventListener('fetch', (e) => {
 
 // Called when the service worker is activated
 self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Activated');
   e.waitUntil(
     (async () => {
       const cacheNames = await caches.keys();
       await Promise.all(
         cacheNames.map((name) => {
           if (name !== cacheName) {
-            console.log(`[Service Worker] Deleting old cache: ${name}`);
             return caches.delete(name);
           }
         })
